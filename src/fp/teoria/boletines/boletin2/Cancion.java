6f65404b8d7eb5454c8e101a5d308c3b5c606dcc
Ubidragon <ubidragon@protonmail.com>
2016-10-26 11:21:46 +0200

    Create Cancion.java

---
+++ src/fp/teoria/boletines/boletin2/Cancion.java
@@ -0,0 +1,16 @@
+//implementar direccion del package
+
+import java.time.Duration
+
+public interface Cancion{
+
+String getID();
+String getNombre();
+Artista getArtista();
+Duration getDuracion();
+Integer gertNumeroPista();
+Integer getPopularidad();
+void setPopularidad(Integer popularidad);
+
+}
+
